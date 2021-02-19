7afba46cb2295ed17333fb80e8b9d1741ff407b3
damonwalker <damon.walker520@gmail.com>
2021-02-19 11:05:29 -0600

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,10 +3,10 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = '';
+let correctAnswer = '';
 let candidateAnswer = '';
 let questions = ["Who was the first American woman in space? ",
  "True or False: 5000 meters = 5 kilometers. " ,
